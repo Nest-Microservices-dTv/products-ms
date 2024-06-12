a94a1136c753df06bf701cbe25e843a13ed143cc
Diam <trejodev@gmail.com>
2024-06-12 13:07:50 +0200

    First commit

---
+++ src/config/envs.ts
@@ -0,0 +1,36 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+  PORT: number;
+  DATABASE_URL: string;
+
+  NATS_SERVERS: string[];
+}
+
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+    DATABASE_URL: joi.string().required(),
+
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
+  })
+  .unknown(true); //Permite otras propiedades
+
+const { error, value } = envsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
+
+const envsVars: EnvVars = value;
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+export const envs = {
+  port: envsVars.PORT,
+  databaseUrl: envsVars.DATABASE_URL,
+
+  natsServers: envsVars.NATS_SERVERS,
+};
---
+++ src/products/products.service.ts
@@ -0,0 +1,97 @@
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
+import { RpcException } from '@nestjs/microservices';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('ProductsService');
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('DB connected');
+  }
+
+  create(createProductDto: CreateProductDto) {
+    return this.product.create({ data: createProductDto });
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+    const { limit, page } = paginationDto;
+
+    const totalPage = await this.product.count({ where: { available: true } });
+    const lastPage = Math.ceil(totalPage / limit);
+
+    return {
+      data: await this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: { available: true },
+      }),
+      meta: {
+        total: totalPage,
+        page,
+        lastPage,
+      },
+    };
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findUnique({
+      where: { id, available: true },
+    });
+
+    if (!product) {
+      throw new RpcException({
+        message: `Product with id #${id} not found`,
+        status: HttpStatus.BAD_REQUEST,
+      });
+    }
+
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const { id: __, ...data } = updateProductDto;
+
+    await this.findOne(id);
+
+    return this.product.update({ where: { id }, data });
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+
+    // return this.product.delete({ where: { id } });
+
+    const product = await this.product.update({
+      where: { id },
+      data: {
+        available: false,
+      },
+    });
+
+    return product;
+  }
+
+  async validateProducts(ids: number[]) {
+    ids = Array.from(new Set(ids)); //Para pulgar mis items duplicados en el array
+
+    const products = await this.product.findMany({
+      //Busca todos los productos con los ids que recibo
+      where: { id: { in: ids } },
+    });
+
+    if (products.length !== ids.length) {
+      //Si no coincide el length de products con el ids.length, significa que hay un id que no existe
+      throw new RpcException({
+        message: 'Some products not found',
+        status: HttpStatus.BAD_REQUEST,
+      });
+    }
+
+    return products;
+  }
+}
